5f753ec9fb2f44fa9371ee152826d957f7c1b6da
Quyen <quyen.le25@student.passerellesnumeriques.org>
2023-09-20 16:51:26 +0700

    Add new file checkbox

---
+++ phone.js
@@ -0,0 +1,12 @@
+
+const courseList = document.querySelector('#add');
+courseList.addEventListener('click',add)
+function add(){
+    var pname = frm.product.value;
+    var img = frm.img.value;
+    var price = frm.price.value;
+    var image = img.slice(12);
+  
+    var app = document.querySelector('.row');
+    app.innerHTML += html;
+}
